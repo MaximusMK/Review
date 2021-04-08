e117a6b2c514b9c02ab461bcceb6882c171e7fb7
Max <68995982+MaximusMK@users.noreply.github.com>
2021-04-08 11:19:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,42 +3,42 @@ const reviews = [
     id: 1,
     name: "Susan Smith",
     job: "web developer",
-    img: "/person-img/person-1.jpg",
+    img: "person-img/person-1.jpg",
     info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur nobis corrupti quidem. Amet at corporis unde, nisi nulla, tempore quod, ea nobis iusto quas maiores. Quam modi fugiat autem quis!"
 },
 {
     id: 2,
     name: "Julia Fox",
     job: "java developer",
-    img: "/person-img/person-2.jpg",
+    img: "person-img/person-2.jpg",
     info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur nobis corrupti quidem. Amet at corporis unde, nisi nulla, tempore quod, ea nobis iusto quas maiores. Quam modi fugiat autem quis!"
 },
 {
     id: 3,
     name: "Alexandra Cooper",
     job: "UX Design",
-    img: "/person-img/person-3.jpg",
+    img: "person-img/person-3.jpg",
     info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur nobis corrupti quidem. Amet at corporis unde, nisi nulla, tempore quod, ea nobis iusto quas maiores. Quam modi fugiat autem quis!"
 },
 {
     id: 4,
     name: "Isabella Cox",
     job: "front-end developer",
-    img: "/person-img/person-4.jpg",
+    img: "person-img/person-4.jpg",
     info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur nobis corrupti quidem. Amet at corporis unde, nisi nulla, tempore quod, ea nobis iusto quas maiores. Quam modi fugiat autem quis!"
 },
 {
     id: 5,
     name: "Anna Moore",
     job: "sysadmin",
-    img: "/person-img/person-5.jpg",
+    img: "person-img/person-5.jpg",
     info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur nobis corrupti quidem. Amet at corporis unde, nisi nulla, tempore quod, ea nobis iusto quas maiores. Quam modi fugiat autem quis!"
 },
 {
     id: 6,
     name: "Julia Anderson",
     job: "project manager",
-    img: "/person-img/person-6.jpg",
+    img: "person-img/person-6.jpg",
     info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur nobis corrupti quidem. Amet at corporis unde, nisi nulla, tempore quod, ea nobis iusto quas maiores. Quam modi fugiat autem quis!"
 },
 ];
